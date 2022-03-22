14c2bd1304a0a6406a45ad772fa8c2a30acd580a
alamine95 <lsow313@gmail.com>
2022-03-22 23:13:47 +0000

    auth

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,26 @@
+const jwt = require('jsonwebtoken')
+const CustomApiError = require('../errors/custom-error')
+
 const login = async (req,res) =>{
-    res.send('Fake Login/Register/Signup Route')
+    const {username,password} = req.body
+    // mongoose validation
+    // Joi
+    // check in the controller
+
+    if(!username || !password){
+        throw new CustomApiError('Please provide email and password', 400)
+    }
+
+    const id = new Date().getDate()
+
+    const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
+
+    res.status(200).json({msg:'user created',token})
 }
 
 const dashboard = async (req,res) =>{
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello Lamine`,secret:`Here is your authorized data, 
-    your lucky number is ${luckyNumber}`})
+    res.status(200).json({msg:`Hello Lamine`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
 }
 
 module.exports = {
