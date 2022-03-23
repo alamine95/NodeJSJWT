beb6f0b608ccd3d1b6fa8d79f8043b12f106af3e
alamine95 <lsow313@gmail.com>
2022-03-23 13:11:17 +0000

    token

--- controllers/main.js
+++ controllers/main.js
@@ -19,8 +19,24 @@ const login = async (req,res) =>{
 }
 
 const dashboard = async (req,res) =>{
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello Lamine`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomApiError('No token provided',401)
+    }
+
+    const token = authHeader.split(' ')[1]
+    
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        console.log(decoded);
+        const luckyNumber = Math.floor(Math.random()*100)
+        res.status(200).json({msg:`Hello, ${decoded.username}`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+    } catch (error) {
+        throw new CustomApiError('Not authorized to access this route',401)
+    }
+
+    
 }
 
 module.exports = {
