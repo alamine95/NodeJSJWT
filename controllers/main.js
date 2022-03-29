f8ec661ba775a01a44a5eb8c43a8ae2dfa54239c
alamine95 <lsow313@gmail.com>
2022-03-29 16:45:28 +0000

    erreurs

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const CustomApiError = require('../errors/custom-error')
+const {BadRequestError} = require('../errors')
 
 const login = async (req,res) =>{
     const {username,password} = req.body
@@ -8,7 +8,7 @@ const login = async (req,res) =>{
     // check in the controller
 
     if(!username || !password){
-        throw new CustomApiError('Please provide email and password', 400)
+        throw new BadRequestError('Please provide email and password')
     }
 
     const id = new Date().getDate()
@@ -19,24 +19,10 @@ const login = async (req,res) =>{
 }
 
 const dashboard = async (req,res) =>{
-    const authHeader = req.headers.authorization;
-
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new CustomApiError('No token provided',401)
-    }
-
-    const token = authHeader.split(' ')[1]
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg:`Hello, ${req.user.username}`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
     
-    try {
-        const decoded = jwt.verify(token,process.env.JWT_SECRET)
-        console.log(decoded);
-        const luckyNumber = Math.floor(Math.random()*100)
-        res.status(200).json({msg:`Hello, ${decoded.username}`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
-    } catch (error) {
-        throw new CustomApiError('Not authorized to access this route',401)
-    }
 
-    
 }
 
 module.exports = {
